a8ea468efd329b243d5dbdc096a210e8edd0a3dd
Bruno Duarte <brunoarduarte@gmail.com>
2022-05-26 17:19:30 -0300

    Category

---
+++ src/main/java/com/brunoarduarte/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.brunoarduarte.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brunoarduarte.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
