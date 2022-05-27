275bb02d3158a1debf0e497adb7a9684b463da46
Bruno Duarte <brunoarduarte@gmail.com>
2022-05-27 16:42:28 -0300

    Subtotal & Total methods

--- src/main/java/com/brunoarduarte/course/entities/Order.java
+++ src/main/java/com/brunoarduarte/course/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
@@ -116,6 +124,5 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 	
 }
